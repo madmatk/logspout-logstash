d9ea15bc709373afd1f85a72ea46f2f5a464094c
Mathias Kriegel <mathiasklev@gmail.com>
2018-03-15 16:30:19 +0100

    Update modules.go

--- modules.go
+++ modules.go
@@ -1,5 +1,8 @@
 package main
 
 import (
+	_ "github.com/gliderlabs/logspout/transports/tcp"
+	_ "github.com/gliderlabs/logspout/transports/tls"
+	_ "github.com/gliderlabs/logspout/transports/udp"
 	_ "github.com/looplab/logspout-logstash"
 )
